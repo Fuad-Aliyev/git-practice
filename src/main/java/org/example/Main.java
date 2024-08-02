7b0d155f70376159e7f2f7c65e7bdec7324f8482
fuad <aliyevfuad.989@gmail.com>
2024-08-02 14:20:37 +0400

    Revert "first commit"
    This reverts commit 4191bff3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         //first commit
-        //second commit
-        //second bad commit
-
-        //this is true
     }
 
 }
